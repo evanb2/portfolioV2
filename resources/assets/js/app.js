23f6a6329b0ebe35ffcdbeed1c9fe1df3c4c9896
Evan Butler <evanbutler@FM-evabut-Mac.local>
2016-10-15 19:02:43 -0700

    adds vue-router and vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,15 +4,22 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import './bootstrap';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import Vuex from 'vuex';
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+import Example from './components/Example.vue';
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('example', Example);
+
+// Vue.use(Vuex);
+Vue.use(VueRouter);
 
 const app = new Vue({
   el: '#app'
